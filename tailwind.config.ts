8f45eb0bd8de1e7314dba7a26d7b8deca1d303da
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2023-10-08 10:39:54 +0700

    Update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,40 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
+const withMT = require('@material-tailwind/react/utils/withMT')
 
 const config = withMT({
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-});
-export default config;
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          padding: '16px',
+          '@media (min-width: 640px)': {
+            maxWidth: '600px',
+            padding: '16px',
+          },
+          '@media (min-width: 768px)': {
+            maxWidth: '768px',
+            padding: '16px',
+          },
+          '@media (min-width: 1024px)': {
+            maxWidth: '1024px',
+            padding: '16px',
+          },
+          '@media (min-width: 1280px)': {
+            maxWidth: '1280px',
+            padding: '20px',
+          },
+        },
+      })
+    },
+  ],
+})
+export default config
