09c935aa8c0f99e761b43543b9d8f85fe1f889b6
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2023-10-15 23:06:44 +0700

    Saved posts page ui

--- src/pages/my-posts.tsx
+++ src/pages/my-posts.tsx
@@ -2,10 +2,12 @@ import Pagination from '~/components/Shared/Pagination'
 import Post from '~/components/Shared/Post'
 import UserLayout from '~/layouts/UserLayout'
 import { type NextPageWithLayout } from './_app'
+import Filter from '~/components/Home/Filter'
 
 const MyPosts: NextPageWithLayout = () => {
   return (
     <div className="container flex flex-col gap-4">
+      <Filter />
       <ul className="flex flex-col gap-4">
         <li>
           <Post />
--- src/pages/saved-posts.tsx
+++ src/pages/saved-posts.tsx
@@ -1,8 +1,35 @@
 import UserLayout from '~/layouts/UserLayout'
 import { type NextPageWithLayout } from './_app'
+import Filter from '~/components/Home/Filter'
+import Post from '~/components/Shared/Post'
+import Pagination from '~/components/Shared/Pagination'
 
 const SavedPosts: NextPageWithLayout = () => {
-  return <div className="container">a</div>
+  return (
+    <div className="container flex flex-col gap-4">
+      <Filter />
+      <ul className="flex flex-col gap-4">
+        <li>
+          <Post />
+        </li>
+        <li>
+          <Post />
+        </li>
+        <li>
+          <Post />
+        </li>
+        <li>
+          <Post />
+        </li>
+        <li>
+          <Post />
+        </li>
+      </ul>
+      <div className="flex justify-center">
+        <Pagination />
+      </div>
+    </div>
+  )
 }
 
 export default SavedPosts
