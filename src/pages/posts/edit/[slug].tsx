57d0ca1e0349fdef2561b23e0944d97863d6af2d
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2023-11-30 15:25:37 +0700

    Page edit post

--- src/pages/posts/edit/[slug].tsx
+++ src/pages/posts/edit/[slug].tsx
@@ -2,13 +2,31 @@ import { HomeIcon } from '@heroicons/react/24/solid'
 import { Breadcrumbs, Typography } from '@material-tailwind/react'
 import Head from 'next/head'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useQuery } from 'react-query'
+import { getPostBySlugFn } from '~/api/postApi'
 import PostForm from '~/components/Posts/PostForm'
+import AuthenticationGuard from '~/components/Shared/AuthenticationGuard'
+import Loading from '~/components/UI/Loading'
 import MainLayout from '~/layouts/MainLayout'
 import { type NextPageWithLayout } from '~/pages/_app'
+import { showToast } from '~/utils/toast'
 
 const EditPost: NextPageWithLayout = () => {
+  const router = useRouter()
+  const { data: postData, isLoading } = useQuery({
+    queryKey: ['post', router.query.slug],
+    queryFn: async () =>
+      await getPostBySlugFn({
+        slug: router.query.slug as string,
+      }),
+    onError: (error: any) => {
+      showToast('error', error.response.data.message)
+    },
+  })
+
   return (
-    <>
+    <AuthenticationGuard>
       <Head>
         <title>Edit Post - Post title</title>
       </Head>
@@ -19,11 +37,15 @@ const EditPost: NextPageWithLayout = () => {
           </Link>
           <Typography>Edit post</Typography>
         </Breadcrumbs>
-        <div className="mx-auto mt-10 h-screen max-w-[800px]">
-          <PostForm />
+        <div className="mx-auto mt-10 max-w-[800px]">
+          {isLoading ? (
+            <Loading />
+          ) : (
+            <PostForm post={postData?.data.post} type="edit" />
+          )}
         </div>
       </div>
-    </>
+    </AuthenticationGuard>
   )
 }
 
