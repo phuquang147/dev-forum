e01671b0d61e9ade83abe9789463ec0b58bb62ea
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2023-11-30 15:05:20 +0700

    Update next-auth data

--- src/resources/types/next-auth.d.ts
+++ src/resources/types/next-auth.d.ts
@@ -1,9 +1,16 @@
-import NextAuth from 'next-auth'
+import 'next-auth'
+import type { TUserRole } from './common.type'
 
 declare module 'next-auth' {
   interface Session {
     user: {
       access_token: string
+      user: {
+        _id: string
+        displayName: string
+        avatar: string
+        role: TUserRole
+      }
     }
   }
 }
