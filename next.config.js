bf9906f682b30b73b928cd874ccf49603224caa7
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2023-11-30 14:55:38 +0700

    Rewrite routes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['firebasestorage.googleapis.com'],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/discuss',
+        destination: '/',
+      },
+      {
+        source: '/bug',
+        destination: '/',
+      },
+      {
+        source: '/news',
+        destination: '/',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
